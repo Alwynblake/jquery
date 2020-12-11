6ed054882e9af394e8d51ef73120ea30311cd764
Alistair Blake <consultations@me.com>
2020-12-11 15:51:30 -0800

    dataRetrieval

--- js/app.js
+++ js/app.js
@@ -16,20 +16,35 @@ $('button').on('click', function() {
   $('ul').toggle('hide');
 });
 
-// 1.DOM manipulation
+// 1.DOM manipulation. Select the element from the DOM
 let $pets = $('.pets');
-let $petTemplate = $('.pet-template');
+// let $petTemplate = $('.pet-template');
 
 // 2.Create the markup/Give it content
-let $newPet = $petTemplate.clone();
-$newPet.removeAttr('class');
-$newPet.text('Allie');
+// let $newPet = $petTemplate.clone();
+// $newPet.removeAttr('class');
+// $newPet.text('Allie');
 
-let $newPetTwo = $petTemplate.clone();
-$newPetTwo.removeAttr('class');
-$newPetTwo.text('Baba');
+// let $newPetTwo = $petTemplate.clone();
+// $newPetTwo.removeAttr('class');
+// $newPetTwo.text('Baba');
 
 // 3.Render it to the DOM
-$pets.append($newPet);
-$pets.append($newPetTwo);
+// $pets.append($newPet);
+// $pets.append($newPetTwo);
 
+// asynchronous call
+$.ajax('./data/pets.json')
+    .then(data => {
+      data.forEach(value => {
+        let $petTemplate = $('.pet-template');
+        let $newPetThree = $petTemplate.clone();
+        $newPetThree.removeAttr('class');
+        $newPetThree.text(value.name);
+        $pets.append($newPetThree);
+
+        console.log(value);
+      })
+    });
+
+console.log('hello');
