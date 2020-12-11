f32a41e0d1c45c03924e4b5f4d354520fb01fb0d
Alistair Blake <consultations@me.com>
2020-12-11 15:20:33 -0800

    first commit

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@
 
 $('button').on('click', function() {
   //getting the text from the button
-  // console.log($(this).text());
+  console.log($(this).text());
 
   //setting the text in the button to something new
   // console.log($(this).text('level'));
@@ -15,4 +15,21 @@ $('button').on('click', function() {
   //toggle
   $('ul').toggle('hide');
 });
+
+// 1.DOM manipulation
+let $pets = $('.pets');
+let $petTemplate = $('.pet-template');
+
+// 2.Create the markup/Give it content
+let $newPet = $petTemplate.clone();
+$newPet.removeAttr('class');
+$newPet.text('Allie');
+
+let $newPetTwo = $petTemplate.clone();
+$newPetTwo.removeAttr('class');
+$newPetTwo.text('Baba');
+
+// 3.Render it to the DOM
+$pets.append($newPet);
+$pets.append($newPetTwo);
 
